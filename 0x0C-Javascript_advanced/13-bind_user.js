a610cd508c9219072e9757c4471f1447671ed808
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-05-20 21:38:55 -0500

    13. Binding + Closure

---
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -0,0 +1,17 @@
+let user = {
+    hobby: 'Calligraphy',
+    favoriteSport: 'Hockey',
+    astrologicalSign: 'Aries',
+    firstName: 'Buillaume',
+    lastName: 'Ialva',
+    location: 'Telaviv',
+    occupation: 'Engineer',
+};
+
+function logWelcomeUser(welcomeString) {
+    console.log(`${welcomeString}, ${firstName}. Your occupation is: ${occupation}`);
+};
+
+let bindLogWelcomeUser = logWelcomeUser.bind(user)
+
+bindLogWelcomeUser('Hello');
